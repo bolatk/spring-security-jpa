8a52dbe2461be3a2b07f86f1a01ca9872ed9096f
Bolat <b.kazybaev@gmail.com>
2020-10-04 23:35:10 +0600

    [feat] Add User entity and repository

---
+++ src/main/java/tech/akaibo/springsecurityjpa/UserRepository.java
@@ -0,0 +1,10 @@
+package tech.akaibo.springsecurityjpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.akaibo.springsecurityjpa.models.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
