b63dc32c49157803de8f0aa4fa589e5fe73aafc9
Bolat <b.kazybaev@gmail.com>
2020-10-04 23:36:06 +0600

    [feat] Add MyUserDetails model and service

---
+++ src/main/java/tech/akaibo/springsecurityjpa/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package tech.akaibo.springsecurityjpa;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tech.akaibo.springsecurityjpa.models.MyUserDetails;
+import tech.akaibo.springsecurityjpa.models.User;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+        return user.map(MyUserDetails::new).get();
+    }
+}
